502be6779740eeef2f03b1057259719fbee93e91
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2018-12-04 10:32:06 +0100

    Use activerecord's drop_table Added docs

--- lib/active_record_temptable.rb
+++ lib/active_record_temptable.rb
@@ -8,6 +8,13 @@ module ActiveRecordTemptable
 
   DEFAULT_TABLE_NAME = 'activerecord_temptable_records'.freeze
 
+
+  # Yields provided relation loaded in temporary table
+  #
+  # @param relation [ActiveRecord::Relation]
+  # @param indexes [Array] add_index will be called for every element in indexes
+  # @param table_name [String] name of table
+  # @return [void]
   def with_temptable(relation, indexes = [], table_name = DEFAULT_TABLE_NAME)
     ActiveRecord::Base.connection_pool.with_connection do |connection|
       connection.transaction do
@@ -17,33 +24,37 @@ module ActiveRecordTemptable
           klass = relation.klass
           yield klass.unscoped.from("#{table_name} AS #{klass.table_name}") if block_given?
         ensure
-          drop_table(connection, table_name)
+          connection.drop_table(table_name, if_exists: true)
         end
       end
     end
   end
 
+  # Creates temp table and loads provided relation into it
+  #
+  # @param connection [ActiveRecord::ConnectionAdapters]
+  # @param relation [ActiveRecord::Relation]
+  # @param table_name [String] name of table
+  # @return [void]
   def create_table(connection, relation, table_name)
     connection.execute(new_table_command(relation, table_name))
   end
 
+  # Calls add_index for every element in array
+  #
+  # @param connection [ActiveRecord::ConnectionAdapters]
+  # @param table_name [String]
+  # @param indexes [Array]
+  # @return [void]
   def create_indexes(connection, table_name, indexes)
     indexes.each do |fields, options|
       connection.add_index(table_name, fields, options || {})
     end
   end
 
-  def drop_table(connection, table_name)
-    connection.execute(drop_table_command(table_name))
-  end
-
   private
 
   def new_table_command(relation, table_name)
     "CREATE TEMPORARY TABLE #{table_name} AS #{relation.to_sql}"
-  end
-
-  def drop_table_command(table_name)
-    "DROP TABLE IF EXISTS #{table_name}"
   end
 end
--- spec/activerecord_temptable_spec.rb
+++ spec/activerecord_temptable_spec.rb
@@ -20,13 +20,14 @@ RSpec.describe ActiveRecordTemptable do
       allow(ActiveRecord::Base.connection_pool).to receive(:with_connection).and_yield(connection)
       allow(connection).to receive(:transaction).and_yield
       allow(connection).to receive(:execute).with(/CREATE TEMP(ORARY)? TABLE/i)
-      allow(connection).to receive(:execute).with(/DROP TABLE IF EXISTS/i)
+      allow(connection).to receive(:drop_table).with(described_class::DEFAULT_TABLE_NAME, if_exists: true)
       begin
         described_class.with_temptable(User.all, []) { raise StandardError.new('Raised error') }
       rescue
       end
       expect(connection).to have_received(:transaction)
-      expect(connection).to have_received(:execute).exactly(2).times
+      expect(connection).to have_received(:execute).exactly(:once)
+      expect(connection).to have_received(:drop_table).exactly(:once)
     end
 
     it 'creates indexes' do
@@ -37,11 +38,13 @@ RSpec.describe ActiveRecordTemptable do
       allow(ActiveRecord::Base.connection_pool).to receive(:with_connection).and_yield(connection)
       allow(connection).to receive(:transaction).and_yield
       allow(connection).to receive(:execute).with(an_instance_of(String)).at_least(:once)
+      allow(connection).to receive(:drop_table).with(tablename, if_exists: true)
       allow(connection).to receive(:add_index).with(tablename, args[0], {})
       allow(connection).to receive(:add_index).with(tablename, args[1][0], args[1][1])
       allow(connection).to receive(:add_index).with(tablename, args[2][0], args[2][1])
       described_class.with_temptable(User.all, args, tablename) {}
       expect(connection).to have_received(:add_index).exactly(3).times
+      expect(connection).to have_received(:drop_table).exactly(:once)
     end
   end
 end
