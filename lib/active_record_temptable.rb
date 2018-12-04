68b55e765bc857bc0be4b88eac4d9949c4df7fd0
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2018-12-04 09:56:15 +0100

    Use connection_pool.with_connection

--- lib/active_record_temptable.rb
+++ lib/active_record_temptable.rb
@@ -7,30 +7,32 @@ module ActiveRecordTemptable
   extend self
 
   def with_temptable(relation, indexes = [], table_name='activerecord_temptable_records')
-    ActiveRecord::Base.connection.transaction do
-    begin
-      create_table(relation, table_name)
-      create_indexes(table_name, indexes) if indexes.any?
-      klass = relation.klass
-      yield klass.unscoped.from("#{table_name} AS #{klass.table_name}") if block_given?
-    ensure
-      drop_table(table_name)
-    end
+    ActiveRecord::Base.connection_pool.with_connection do |connection|
+      connection.transaction do
+      begin
+        create_table(connection, relation, table_name)
+        create_indexes(connection, table_name, indexes) if indexes.any?
+        klass = relation.klass
+        yield klass.unscoped.from("#{table_name} AS #{klass.table_name}") if block_given?
+      ensure
+        drop_table(connection, table_name)
+      end
+      end
     end
   end
 
-  def create_table(relation, table_name)
-    ActiveRecord::Base.connection.execute(new_table_command(relation, table_name))
+  def create_table(connection, relation, table_name)
+    connection.execute(new_table_command(relation, table_name))
   end
 
-  def create_indexes(table_name, indexes)
+  def create_indexes(connection, table_name, indexes)
     indexes.each do |fields, options|
-      ActiveRecord::Base.connection.add_index(table_name, fields, options || {})
+      connection.add_index(table_name, fields, options || {})
     end
   end
 
-  def drop_table(table_name)
-    ActiveRecord::Base.connection.execute(drop_table_command(table_name))
+  def drop_table(connection, table_name)
+    connection.execute(drop_table_command(table_name))
   end
 
   private
--- spec/activerecord_temptable_spec.rb
+++ spec/activerecord_temptable_spec.rb
@@ -16,24 +16,32 @@ RSpec.describe ActiveRecordTemptable do
     end
 
     it 'creates and drops temp table' do
-      allow(ActiveRecord::Base.connection).to receive(:execute).with(/CREATE TEMP(ORARY)? TABLE/i)
-      allow(ActiveRecord::Base.connection).to receive(:execute).with(/DROP TABLE IF EXISTS/i)
+      connection = double
+      allow(ActiveRecord::Base.connection_pool).to receive(:with_connection).and_yield(connection)
+      allow(connection).to receive(:transaction).and_yield
+      allow(connection).to receive(:execute).with(/CREATE TEMP(ORARY)? TABLE/i)
+      allow(connection).to receive(:execute).with(/DROP TABLE IF EXISTS/i)
       begin
         described_class.with_temptable(User.all, []) { raise StandardError.new('Raised error') }
       rescue
       end
-      expect(ActiveRecord::Base.connection).to have_received(:execute).exactly(2).times
+      expect(connection).to have_received(:transaction)
+      expect(connection).to have_received(:execute).exactly(2).times
     end
 
     it 'creates indexes' do
       args = [:id, [:email, unique: true], [[:id, :email], unique: true]]
       tablename = 'test_temp_table'
 
-      allow(ActiveRecord::Base.connection).to receive(:add_index).with(tablename, args[0], {})
-      allow(ActiveRecord::Base.connection).to receive(:add_index).with(tablename, args[1][0], args[1][1])
-      allow(ActiveRecord::Base.connection).to receive(:add_index).with(tablename, args[2][0], args[2][1])
+      connection = double
+      allow(ActiveRecord::Base.connection_pool).to receive(:with_connection).and_yield(connection)
+      allow(connection).to receive(:transaction).and_yield
+      allow(connection).to receive(:execute).with(an_instance_of(String)).at_least(:once)
+      allow(connection).to receive(:add_index).with(tablename, args[0], {})
+      allow(connection).to receive(:add_index).with(tablename, args[1][0], args[1][1])
+      allow(connection).to receive(:add_index).with(tablename, args[2][0], args[2][1])
       described_class.with_temptable(User.all, args, tablename) {}
-      expect(ActiveRecord::Base.connection).to have_received(:add_index).exactly(3).times
+      expect(connection).to have_received(:add_index).exactly(3).times
     end
   end
 end
